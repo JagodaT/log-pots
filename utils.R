9b92d49e7c06d6488872d9451554f0199734ac5a
Thomás <thomasjagoda@hotmail.com>
2021-08-12 16:29:56 -0300

    clean up

--- utils.R
+++ utils.R
@@ -26,28 +26,4 @@ anti_log_pot <- function(taper_pct = 50, pot=25, mid=0.85) {
   (a*b^taper - a)*pot
 }
 
-# real_pot = 150
-# pot=500
-# load_resistor=220
-# 
-# df_plot_data <- tibble(taper_pct=seq(0, 100, 0.1))
-# df_plot_data <- df_plot_data %>%
-#   mutate(tru_anti_log_pot = anti_log_pot(taper_pct, pot=real_pot),
-#          aprox_anti_log_pot = anti_log_aprox(taper_pct, 
-#                                                  pot=pot, load_res=load_resistor))
-# 
-# p1 <- df_plot_data %>%
-#   rename(Approximation = aprox_anti_log_pot, 
-#          Real = tru_anti_log_pot, rotation = taper_pct ) %>%
-#   pivot_longer(c(Real, Approximation), names_to = "potentiometre",
-#                values_to = "resistance") %>%
-#   ggplot(aes(rotation, resistance, colour=potentiometre)) + 
-#   geom_line() + 
-#   theme_minimal() +
-#   labs(title= ' Real vs Approx. Anti-log', x="Rotation %", y="Resistance (kohms)") +
-#   theme(legend.title = element_blank())
-#   # geom_function(fun=res_divider, args=list(pot=200, resistor =20)) +
-#   # geom_function(fun=log_pot, args=list(pot=150), color="red") 
-# p1
-# ggplotly(p1) 
 
